a34880b729e549e46fd5f0f3a77b62107c8b88aa
Raphael Panic <raphael.panic@gmx.at>
2023-03-11 16:42:10 +0100

    Finished first MultiSig test

--- src/multisig/multisigv2.test.ts
+++ src/multisig/multisigv2.test.ts
@@ -1,305 +1,314 @@
 import {
-    shutdown,
-    PrivateKey,
-    Field,
-    Mina,
-    MerkleTree,
-    AccountUpdate,
-    UInt64,
-    Bool,
-    PublicKey,
-    Signature,
+  shutdown,
+  PrivateKey,
+  Field,
+  Mina,
+  MerkleTree,
+  AccountUpdate,
+  UInt64,
+  Bool,
+  PublicKey,
+  Signature,
 } from 'snarkyjs';
-import {beforeEach, afterAll, it, expect} from '@jest/globals';
-import {describeNetworkAware} from '../JestExtensions';
-import {MultiSigContract} from './multisigv2';
-import {TransactionId} from '../utils';
+import { beforeEach, afterAll, it, expect } from '@jest/globals';
+import { describeNetworkAware } from '../JestExtensions';
+import { MultiSigContract } from './multisigv2';
+import { TransactionId } from '../utils';
 import {
-    MultiSigProgram,
-    MultisigProgramProof,
-    MultiSigProgramState,
-    MultiSigProgramStateTransition,
+  MultiSigProgram,
+  MultiSigProgramNoProofs,
+  MultisigProgramProof,
+  MultiSigProgramState,
+  MultiSigProgramStateTransition,
 } from './multisigv2program';
 import {
-    MULTISIG_MERKLE_HEIGHT,
-    MultiSigMerkleWitness,
-    Proposal,
-    ProposalState,
-    SignerState,
+  MULTISIG_MERKLE_HEIGHT,
+  MultiSigMerkleWitness,
+  Proposal,
+  ProposalState,
+  SignerState,
 } from './model';
-import {tic, toc} from '../tictoc';
+import { tic, toc } from '../tictoc';
 
 describeNetworkAware('Multisig - E2E', (context) => {
-    let accounts: PrivateKey[],
-        deployToBerkeley = context.berkeley;
+  let accounts: PrivateKey[],
+    deployToBerkeley = context.berkeley;
 
-    async function deployAndFundMultisig(
-        signers: MerkleTree,
-        signersLength: number,
-        k: number
-    ): Promise<{
-        tx: TransactionId;
-        pk: PrivateKey;
-        instance: MultiSigContract;
-    }> {
-        let pk = PrivateKey.random();
-        let deployArgs = await context.getDeployArgs(pk, MultiSigContract);
+  async function deployAndFundMultisig(
+    signers: MerkleTree,
+    signersLength: number,
+    k: number
+  ): Promise<{
+    tx: TransactionId;
+    pk: PrivateKey;
+    instance: MultiSigContract;
+  }> {
+    let pk = PrivateKey.random();
+    let deployArgs = await context.getDeployArgs(pk, MultiSigContract);
 
-        console.log(deployArgs)
+    let contract = new MultiSigContract(pk.toPublicKey());
 
-        let contract = new MultiSigContract(pk.toPublicKey());
+    console.log('Init with k = ', k);
 
-        let tx = await Mina.transaction(
-            {sender: accounts[0].toPublicKey(), fee: 0.01 * 1e9},
-            () => {
-                //Pay account creation fee
-                AccountUpdate.fundNewAccount(
-                    accounts[0].toPublicKey()
-                ).requireSignature();
+    let tx = await Mina.transaction(
+      { sender: accounts[0].toPublicKey(), fee: 0.01 * 1e9 },
+      () => {
+        //Pay account creation fee
+        AccountUpdate.fundNewAccount(
+          accounts[0].toPublicKey()
+        ).requireSignature();
 
-                //Deploy Contract
-                contract.deployContract(deployArgs, context.editPermission);
-                if (!context.proofs) {
-                    contract.requireSignature();
-                }
-                console.log('Init with k = ', k);
-
-                //Setup state
-                contract.setup(signers.getRoot(), Field(signersLength), Field(k));
-                if (!context.proofs) {
-                    contract.requireSignature();
-                }
-
-                //Fund contract with initial $MINA
-                let sendAu = AccountUpdate.create(accounts[0].toPublicKey());
-                sendAu.send({
-                    to: contract.address,
-                    amount: UInt64.from(3 * 1e9),
-                });
-                sendAu.requireSignature();
-            }
-        );
-
-        await context.signOrProve(tx, accounts[0], [pk, accounts[0]]);
-
-        let txId = await tx.send();
-
-        return {tx: txId, pk, instance: contract};
-    }
-
-    beforeAll(async () => {
-        await context.before();
-
-        accounts = context.accounts;
-
-        console.log(
-            accounts
-                .map((x, i) => i + ': ' + x.toPublicKey().toBase58())
-                .reduce((a, b, i) => a + '\n' + b)
-        );
-    });
-
-    afterAll(() => {
-        setInterval(shutdown, 0);
-    });
-
-    let contract: MultiSigContract, contractPk: PrivateKey, signers: MerkleTree;
-
-    beforeEach(async () => {
-        let numSigners = 3;
-
-        signers = new MerkleTree(MULTISIG_MERKLE_HEIGHT);
-        for (let i = 0; i < numSigners; i++) {
-            let signer = accounts[2 + i].toPublicKey();
-            signers.setLeaf(
-                signer.x.toBigInt(),
-                new SignerState({
-                    pubkey: signer,
-                    voted: Bool(false),
-                }).hash()
-            );
+        //Deploy Contract
+        contract.deployContract(deployArgs, context.editPermission);
+        if (!context.proofs) {
+          contract.requireSignature();
         }
 
-        if(context.proofs){
-            tic('Compiling MultiSigProgram');
-            await MultiSigProgram.compile();
-            toc();
+        //Setup state
+        contract.setup(signers.getRoot(), Field(signersLength), Field(k));
+        if (!context.proofs) {
+          contract.requireSignature();
         }
 
-        let {tx, pk, instance} = await deployAndFundMultisig(
-            signers,
-            numSigners,
-            2
-        );
-        contract = instance;
-        contractPk = pk;
+        //Fund contract with initial $MINA
+        let sendAu = AccountUpdate.create(accounts[0].toPublicKey());
+        sendAu.send({
+          to: contract.address,
+          amount: UInt64.from(3 * 1e9),
+        });
+        sendAu.requireSignature();
+      }
+    );
 
-        await tx.wait();
+    await context.signOrProve(tx, accounts[0], [pk, accounts[0]]);
+
+    let txId = await tx.send();
+
+    return { tx: txId, pk, instance: contract };
+  }
+
+  beforeAll(async () => {
+    await context.before();
+
+    accounts = context.accounts;
+
+    console.log(
+      accounts
+        .map((x, i) => i + ': ' + x.toPublicKey().toBase58())
+        .reduce((a, b, i) => a + '\n' + b)
+    );
+  });
+
+  afterAll(() => {
+    setInterval(shutdown, 0);
+  });
+
+  let contract: MultiSigContract, contractPk: PrivateKey, signers: MerkleTree;
+
+  beforeEach(async () => {
+    let numSigners = 3;
+
+    signers = new MerkleTree(MULTISIG_MERKLE_HEIGHT);
+    for (let i = 0; i < numSigners; i++) {
+      let signer = accounts[2 + i].toPublicKey();
+      signers.setLeaf(
+        signer.x.toBigInt(),
+        new SignerState({
+          pubkey: signer,
+          voted: Bool(false),
+        }).hash()
+      );
+    }
+
+    if (context.proofs) {
+      tic('Compiling MultiSigProgram');
+      await MultiSigProgram.compile();
+      toc();
+    }
+
+    let { tx, pk, instance } = await deployAndFundMultisig(
+      signers,
+      numSigners,
+      2
+    );
+    contract = instance;
+    contractPk = pk;
+
+    await tx.wait();
+  });
+
+  function computePublicInput(
+    proposal: Proposal,
+    votes: [Field, Field],
+    signer: PublicKey,
+    vote: Bool
+  ): MultiSigProgramStateTransition {
+    let state1 = new ProposalState({
+      proposal,
+      votes: votes.slice(),
+      signerStateRoot: signers.getRoot(),
     });
 
-    function computePublicInput(
-        proposal: Proposal,
-        votes: [Field, Field],
-        signer: PublicKey,
-        vote: Bool
-    ): MultiSigProgramStateTransition {
-        let state1 = new ProposalState({
-            proposal,
-            votes,
-            signerStateRoot: signers.getRoot(),
-        });
+    signers.setLeaf(
+      signer.x.toBigInt(),
+      new SignerState({
+        pubkey: signer,
+        voted: Bool(true),
+      }).hash()
+    );
 
-        signers.setLeaf(
-            signer.x.toBigInt(),
-            new SignerState({
-                pubkey: signer,
-                voted: Bool(true),
-            }).hash()
-        );
+    votes[vote.toBoolean() ? 0 : 1] = votes[vote.toBoolean() ? 0 : 1].add(1);
 
-        votes[vote.toBoolean() ? 0 : 1] = votes[vote.toBoolean() ? 0 : 1].add(1);
+    let state2 = new ProposalState({
+      proposal,
+      votes: votes.slice(),
+      signerStateRoot: signers.getRoot(),
+    });
 
-        let state2 = new ProposalState({
-            proposal,
-            votes,
-            signerStateRoot: signers.getRoot(),
-        });
+    return new MultiSigProgramStateTransition({
+      from: new MultiSigProgramState({
+        state: state1,
+      }),
+      to: new MultiSigProgramState({
+        state: state2,
+      }),
+    });
+  }
 
-        return new MultiSigProgramStateTransition({
-            from: new MultiSigProgramState({
-                state: state1,
-            }),
-            to: new MultiSigProgramState({
-                state: state2,
-            }),
-        });
+  async function proveApproval(
+    signer: PrivateKey,
+    vote: Bool,
+    transition: MultiSigProgramStateTransition,
+    witness: MultiSigMerkleWitness
+  ): Promise<MultisigProgramProof> {
+    let signature1 = Signature.create(signer, [
+      transition.from.state.proposal.hash(),
+      Bool(true).toField(),
+    ]);
+
+    if (context.proofs) {
+      tic('Creating approval proof');
+      let proof = await MultiSigProgram.approve(
+        transition,
+        signer.toPublicKey(),
+        signature1,
+        Bool(true),
+        witness
+      );
+      toc();
+      return proof;
+    } else {
+      MultiSigProgramNoProofs.approve(
+        transition,
+        signer.toPublicKey(),
+        signature1,
+        Bool(true),
+        witness
+      );
+
+      return new MultisigProgramProof({
+        maxProofsVerified: 2,
+        proof: '',
+        publicInput: transition,
+      });
+    }
+  }
+
+  async function mergeProofs(
+    proof1: MultisigProgramProof,
+    proof2: MultisigProgramProof
+  ): Promise<MultisigProgramProof> {
+    let transition = new MultiSigProgramStateTransition({
+      from: proof1.publicInput.from,
+      to: proof2.publicInput.to,
+    });
+
+    if (context.proofs) {
+      tic('Merging MultiSig proofs');
+      let mergedProof = await MultiSigProgram.merge(transition, proof1, proof2);
+      toc();
+      return mergedProof;
+    } else {
+      MultiSigProgramNoProofs.merge(
+        transition,
+        proof1.publicInput,
+        proof2.publicInput
+      );
+      return new MultisigProgramProof({
+        maxProofsVerified: 2,
+        proof: '',
+        publicInput: transition,
+      });
+    }
+  }
+
+  it(' enabled Test Approve', async () => {
+    let proposalAmount = UInt64.from(1000);
+    let proposal = new Proposal({
+      receiver: accounts[8].toPublicKey(),
+      amount: proposalAmount,
+    });
+    let votes: [Field, Field] = [Field(0), Field(0)];
+
+    let transition1 = computePublicInput(
+      proposal,
+      votes,
+      accounts[2].toPublicKey(),
+      Bool(true)
+    );
+    let witness1 = signers.getWitness(accounts[2].toPublicKey().x.toBigInt());
+    expect(votes[0]).toEqual(Field(1));
+    let transition2 = computePublicInput(
+      proposal,
+      votes,
+      accounts[3].toPublicKey(),
+      Bool(true)
+    );
+    let witness2 = signers.getWitness(accounts[3].toPublicKey().x.toBigInt());
+
+    let proof1 = await proveApproval(
+      accounts[2],
+      Bool(true),
+      transition1,
+      new MultiSigMerkleWitness(witness1)
+    );
+    let proof2 = await proveApproval(
+      accounts[3],
+      Bool(true),
+      transition2,
+      new MultiSigMerkleWitness(witness2)
+    );
+
+    let proof = await mergeProofs(proof1, proof2);
+
+    if (context.proofs) {
+      console.log(proof.toJSON());
     }
 
-    async function proveApproval(
-        signer: PrivateKey,
-        vote: Bool,
-        transition: MultiSigProgramStateTransition,
-        witness: MultiSigMerkleWitness
-    ): Promise<MultisigProgramProof> {
+    expect(proof).toBeDefined();
+    console.log('Proofs generated!');
 
-        let signature1 = Signature.create(signer, [
-            transition.from.state.proposal.hash(),
-            Bool(true).toField(),
-        ]);
-        tic('Creating approval proof');
-        let proof = await MultiSigProgram.approve(
-            transition,
-            signer.toPublicKey(),
-            signature1,
-            Bool(true),
-            witness
-        );
-        toc();
-        return proof;
-
-    }
-
-    async function mergeProofs(
-        proof1: MultisigProgramProof,
-        proof2: MultisigProgramProof
-    ): Promise<MultisigProgramProof> {
-
-        let transition = new MultiSigProgramStateTransition({
-            from: proof1.publicInput.from,
-            to: proof2.publicInput.to,
-        });
-        tic('Merging MultiSig proofs');
-        let mergedProof = await MultiSigProgram.merge(transition, proof1, proof2);
-        toc();
-        return mergedProof;
-
-    }
-
-    it(' enabled Test Approve', async () => {
-
-        let proposalAmount = UInt64.from(1000);
-        let proposal = new Proposal({
-            receiver: accounts[8].toPublicKey(),
-            amount: proposalAmount,
-        });
-        let votes: [Field, Field] = [Field(0), Field(0)];
-
-        let transition1 = computePublicInput(
-            proposal,
-            votes,
-            accounts[2].toPublicKey(),
-            Bool(true)
-        );
-        let witness1 = signers.getWitness(accounts[2].toPublicKey().x.toBigInt());
-        expect(votes[0]).toEqual(Field(1));
-        let transition2 = computePublicInput(
-            proposal,
-            votes,
-            accounts[3].toPublicKey(),
-            Bool(true)
-        );
-        let witness2 = signers.getWitness(accounts[3].toPublicKey().x.toBigInt());
-
-        let proof: MultisigProgramProof;
-
-        if (context.proofs) {
-
-            let proof1 = await proveApproval(
-                accounts[2],
-                Bool(true),
-                transition1,
-                new MultiSigMerkleWitness(witness1)
-            );
-            let proof2 = await proveApproval(
-                accounts[3],
-                Bool(true),
-                transition2,
-                new MultiSigMerkleWitness(witness2)
-            );
-
-            proof = await mergeProofs(proof1, proof2);
-
-        } else {
-
-            let mergedTransition = new MultiSigProgramStateTransition({
-                from: transition1.from,
-                to: transition2.to,
-            });
-
-            proof = MultisigProgramProof.fromJSON({
-                maxProofsVerified: 2,
-                proof: '',
-                publicInput: MultiSigProgramStateTransition.toFields(
-                    mergedTransition
-                ).map((x) => x.toString()),
-            });
+    let tx = await Mina.transaction(
+      { sender: accounts[0].toPublicKey() },
+      () => {
+        contract.approveWithProof(proof);
+        if (!context.proofs) {
+          contract.requireSignature();
         }
+      }
+    );
+    await context.signOrProve(tx, accounts[0], [contractPk]);
+    await (await tx.send()).wait();
 
-        console.log(proof.toJSON());
+    let contractAccount = await context.getAccount(contract.address);
+    let receiverAcccount = await context.getAccount(accounts[8].toPublicKey());
 
-        expect(proof).toBeDefined();
-        console.log('Proofs generated!');
-
-        let tx = await Mina.transaction(
-            {sender: accounts[0].toPublicKey()},
-            () => {
-                contract.approveWithProof(proof);
-                if (!context.proofs) {
-                    contract.requireSignature();
-                }
-            }
-        );
-        await context.signOrProve(tx, accounts[0], [contractPk]);
-
-        let contractAccount = await context.getAccount(contract.address);
-        let receiverAcccount = await context.getAccount(accounts[8].toPublicKey());
-
-        expect(contractAccount.balance).toEqual(
-            UInt64.from(3 * 1e9).sub(proposalAmount)
-        );
-        expect(receiverAcccount.balance).toEqual(
-            UInt64.from(1000n * 10n ** 9n).add(proposalAmount)
-        );
-        //TODO Unfunded account
-    });
-})(false, true);
+    expect(contractAccount.balance).toEqual(
+      UInt64.from(3 * 1e9).sub(proposalAmount)
+    );
+    expect(receiverAcccount.balance).toEqual(
+      UInt64.from(1000n * 10n ** 9n).add(proposalAmount)
+    );
+    //TODO Unfunded account
+  });
+})(false, false);
--- src/multisig/multisigv2program.ts
+++ src/multisig/multisigv2program.ts
@@ -1,121 +1,156 @@
 import {
-    Bool,
-    Circuit,
-    Experimental,
-    Field,
-    isReady,
-    MerkleMapWitness,
-    Proof,
-    PublicKey,
-    SelfProof,
-    Signature,
-    Struct,
+  Bool,
+  Circuit,
+  Experimental,
+  Field,
+  isReady,
+  MerkleMapWitness,
+  Proof,
+  PublicKey,
+  SelfProof,
+  Signature,
+  Struct,
 } from 'snarkyjs';
 import ZkProgram = Experimental.ZkProgram;
 import {
-    MultiSigMerkleWitness,
-    Proposal,
-    ProposalState,
-    SignerState,
+  MultiSigMerkleWitness,
+  Proposal,
+  ProposalState,
+  SignerState,
 } from './model';
-import {MerkleMapUtils} from '../utils';
+import { MerkleMapUtils } from '../utils';
 
 export class MultiSigProgramState extends Struct({
-    state: ProposalState,
+  state: ProposalState,
 }) {
-    equals(other: MultiSigProgramState): Bool {
-        return this.state.hash().equals(other.state.hash());
-    }
+  equals(other: MultiSigProgramState): Bool {
+    return this.state.hash().equals(other.state.hash());
+  }
 }
 
 export class MultiSigProgramStateTransition extends Struct({
-    from: MultiSigProgramState,
-    to: MultiSigProgramState,
-}) {
-}
+  from: MultiSigProgramState,
+  to: MultiSigProgramState,
+}) {}
+
+export const MultiSigProgramNoProofs = {
+  approve: function (
+    publicInput: MultiSigProgramStateTransition,
+    signer: PublicKey,
+    signature: Signature,
+    vote: Bool,
+    signerWitness: MultiSigMerkleWitness
+  ) {
+    let proposalState = publicInput.from.state.deepCopy();
+
+    //Program
+    signature
+      .verify(signer, [proposalState.proposal.hash(), vote.toField()])
+      .assertTrue('Signature not valid');
+
+    let signerState = new SignerState({
+      pubkey: signer,
+      voted: Bool(false),
+    });
+
+    signerWitness
+      .calculateIndex()
+      .assertEquals(signer.x, 'Witness index not equal to signer.x');
+    signerWitness
+      .calculateRoot(signerState.hash())
+      .assertEquals(
+        proposalState.signerStateRoot,
+        'Signer root not equal to proposalstate'
+      );
+
+    signerState.voted = Bool(true);
+    proposalState.signerStateRoot = signerWitness.calculateRoot(
+      signerState.hash()
+    );
+
+    //Change vote
+    proposalState.votes[0] = proposalState.votes[0].add(
+      Circuit.if(vote, Field(1), Field(0))
+    );
+
+    proposalState.votes[1] = proposalState.votes[1].add(
+      Circuit.if(vote, Field(0), Field(1))
+    );
+
+    Circuit.log('P1', proposalState);
+
+    publicInput.to.state
+      .hash()
+      .assertEquals(
+        proposalState.hash(),
+        'Resulting ProposalState not matching'
+      );
+  },
+
+  merge: function (
+    publicInput: MultiSigProgramStateTransition,
+    proof1: MultiSigProgramStateTransition,
+    proof2: MultiSigProgramStateTransition
+  ) {
+    publicInput.from
+      .equals(proof1.from)
+      .assertTrue('PublicInput -> Proof1 transition not correct');
+    proof1.to
+      .equals(proof2.from)
+      .assertTrue('Proof1 -> Proof2 transition not correct');
+    proof2.to
+      .equals(publicInput.to)
+      .assertTrue('Proof2 -> PublicInput transition not correct');
+  },
+};
 
 export const MultiSigProgram = ZkProgram({
-    publicInput: MultiSigProgramStateTransition,
+  publicInput: MultiSigProgramStateTransition,
 
-    methods: {
-        approve: {
-            privateInputs: [PublicKey, Signature, Bool, MultiSigMerkleWitness],
-            method(
-                publicInput: MultiSigProgramStateTransition,
-                signer: PublicKey,
-                signature: Signature,
-                vote: Bool,
-                signerWitness: MultiSigMerkleWitness
-            ) {
-                let proposalState = publicInput.from.state.deepCopy();
-
-                //Program
-                signature
-                    .verify(signer, [proposalState.proposal.hash(), vote.toField()])
-                    .assertTrue('Signature not valid');
-
-                let signerState = new SignerState({
-                    pubkey: signer,
-                    voted: Bool(false),
-                });
-
-                signerWitness
-                    .calculateIndex()
-                    .assertEquals(signer.x, 'Witness index not equal to signer.x');
-                signerWitness
-                    .calculateRoot(signerState.hash())
-                    .assertEquals(
-                        proposalState.signerStateRoot,
-                        'Signer root not equal to proposalstate'
-                    );
-
-                signerState.voted = Bool(true);
-                proposalState.signerStateRoot = signerWitness.calculateRoot(
-                    signerState.hash()
-                );
-
-                //Change vote
-                proposalState.votes[0] = proposalState.votes[0].add(
-                    Circuit.if(vote, Field(1), Field(0))
-                );
-
-                proposalState.votes[1] = proposalState.votes[1].add(
-                    Circuit.if(vote, Field(0), Field(1))
-                );
-
-                publicInput.to.state
-                    .hash()
-                    .assertEquals(
-                        proposalState.hash(),
-                        'Resulting ProposalState not matching'
-                    );
-            },
-        },
-        merge: {
-            privateInputs: [SelfProof, SelfProof],
-            method(
-                publicInput: MultiSigProgramStateTransition,
-                proof1: SelfProof<MultiSigProgramStateTransition>,
-                proof2: SelfProof<MultiSigProgramStateTransition>
-            ) {
-                proof1.verify();
-                proof2.verify();
-
-                publicInput.from
-                    .equals(proof1.publicInput.from)
-                    .assertTrue('PublicInput -> Proof1 transition not correct');
-                proof1.publicInput.to
-                    .equals(proof2.publicInput.from)
-                    .assertTrue('Proof1 -> Proof2 transition not correct');
-                proof2.publicInput.to
-                    .equals(publicInput.to)
-                    .assertTrue('Proof2 -> PublicInput transition not correct');
-            },
-        },
+  methods: {
+    approve: {
+      privateInputs: [PublicKey, Signature, Bool, MultiSigMerkleWitness],
+      method(
+        publicInput: MultiSigProgramStateTransition,
+        signer: PublicKey,
+        signature: Signature,
+        vote: Bool,
+        signerWitness: MultiSigMerkleWitness
+      ) {
+        MultiSigProgramNoProofs.approve(
+          publicInput,
+          signer,
+          signature,
+          vote,
+          signerWitness
+        );
+      },
     },
+    merge: {
+      privateInputs: [SelfProof, SelfProof],
+      method(
+        publicInput: MultiSigProgramStateTransition,
+        proof1: SelfProof<MultiSigProgramStateTransition>,
+        proof2: SelfProof<MultiSigProgramStateTransition>
+      ) {
+        proof1.verify();
+        proof2.verify();
+
+        publicInput.from
+          .equals(proof1.publicInput.from)
+          .assertTrue('PublicInput -> Proof1 transition not correct');
+        proof1.publicInput.to
+          .equals(proof2.publicInput.from)
+          .assertTrue('Proof1 -> Proof2 transition not correct');
+        proof2.publicInput.to
+          .equals(publicInput.to)
+          .assertTrue('Proof2 -> PublicInput transition not correct');
+      },
+    },
+  },
 });
 
 export class MultisigProgramProof extends Proof<MultiSigProgramStateTransition> {
-    static publicInputType = MultiSigProgramStateTransition;
-    static tag = () => MultiSigProgram;
+  static publicInputType = MultiSigProgramStateTransition;
+  static tag = () => MultiSigProgram;
 }
